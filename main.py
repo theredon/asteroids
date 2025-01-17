a34befaad40357ddf0a4f5681c47b1ecd695a262
Theredon <theredon@gmail.com>
2025-01-17 15:57:38 -0700

    Cleaning up original print statments

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
