c57afd7d7dbcb94ba59ce7379bfefb6559d7c389
Theredon <theredon@gmail.com>
2025-01-18 15:43:44 -0700

    Added circle and player functions and built a player

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
