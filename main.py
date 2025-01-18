70fced70216f0a563cb49d9c81bbb2d61f435499
Theredon <theredon@gmail.com>
2025-01-18 16:06:24 -0700

    Added groups for game objects and updated while loop.  Added whitespace for readability

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from player import Player
 
 def main():
     pygame.init()
-    game_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for object in updateable:
+            object.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
